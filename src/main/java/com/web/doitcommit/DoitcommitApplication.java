97e734a04889df1c5e78e962c6c6fc8cca7f3476
hyeongwoo-LEE <lhw970804@naver.com>
2021-12-30 23:32:32 +0900

    init

---
+++ src/main/java/com/web/doitcommit/DoitcommitApplication.java
@@ -0,0 +1,13 @@
+package com.web.doitcommit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoitcommitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoitcommitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web/doitcommit/DoitcommitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.doitcommit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoitcommitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
