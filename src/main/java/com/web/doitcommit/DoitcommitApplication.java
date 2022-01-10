ffaa2b7b8d481fb2f6ae198b4140be17e653f5ed
hyeongwoo-LEE <77663506+hyeongwoo-LEE@users.noreply.github.com>
2022-01-11 02:20:42 +0900

    로그인 기능 배포

--- src/main/java/com/web/doitcommit/DoitcommitApplication.java
+++ src/main/java/com/web/doitcommit/DoitcommitApplication.java
@@ -2,7 +2,6 @@ package com.web.doitcommit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
