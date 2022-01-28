f997309755cf12162523dd5870a497131f75d5b9
corazon4815 <69441691+corazon4815@users.noreply.github.com>
2022-01-28 16:53:09 +0900

    feat : 멤버 정보 조회 API swagger 적용

--- src/main/java/com/web/doitcommit/dto/member/MemberInfoDto.java
+++ src/main/java/com/web/doitcommit/dto/member/MemberInfoDto.java
@@ -2,6 +2,7 @@ package com.web.doitcommit.dto.member;
 
 import com.web.doitcommit.domain.interestTech.InterestTech;
 import com.web.doitcommit.domain.member.Member;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,26 +10,35 @@ import lombok.NoArgsConstructor;
 import java.util.HashSet;
 import java.util.Set;
 
+@Schema(description = "멤버 정보 dto")
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class MemberInfoDto {
 
+    @Schema(description = "닉네임", nullable = true)
     private String nickname;
 
+    @Schema(description = "이메일", nullable = true)
     private String email;
 
+    @Schema(description = "관심기술정보", nullable = true)
     private Set<InterestTech> interestTechSet = new HashSet<InterestTech>();
 
+    @Schema(description = "포지션", nullable = true)
     private String position;
 
+    @Schema(description = "깃허브 URL", nullable = true)
     private String githubUrl;
 
+    @Schema(description = "URL1", nullable = true)
     private String url1;
 
+    @Schema(description = "URL2", nullable = true)
     private String url2;
 
+    @Schema(description = "프로필 사진 URL", nullable = true)
     private String pictureUrl;
 
     public MemberInfoDto(Member member) {
