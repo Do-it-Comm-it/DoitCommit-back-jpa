911774a6664ad077c668fd57c87b32619bba4957
corazon4815 <69441691+corazon4815@users.noreply.github.com>
2022-01-28 17:02:22 +0900

    feat : MemberUpdateDto API swagger 적용

--- src/main/java/com/web/doitcommit/controller/MemberController.java
+++ src/main/java/com/web/doitcommit/controller/MemberController.java
@@ -11,7 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
--- src/main/java/com/web/doitcommit/dto/member/MemberUpdateDto.java
+++ src/main/java/com/web/doitcommit/dto/member/MemberUpdateDto.java
@@ -1,6 +1,7 @@
 package com.web.doitcommit.dto.member;
 
 import com.web.doitcommit.domain.interestTech.InterestTech;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,30 +11,41 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.HashSet;
 import java.util.Set;
 
+@Schema(description = "멤버 업데이트 dto")
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class MemberUpdateDto {
 
+    @Schema(description = "파일정보", nullable = true)
     private MultipartFile file;
 
+    @Schema(description = "닉네임", nullable = true)
     private String nickname;
 
+    @Schema(description = "이메일", nullable = true)
     private String email;
 
+    @Schema(description = "관심기술정보", nullable = true)
     private Set<InterestTech> interestTechSet = new HashSet<InterestTech>();
 
+    @Schema(description = "포지션", nullable = true)
     private String position;
 
+    @Schema(description = "권한", nullable = true)
     private String role;
 
+    @Schema(description = "깃허브 URL", nullable = true)
     private String githubUrl;
 
+    @Schema(description = "URL1", nullable = true)
     private String url1;
 
+    @Schema(description = "URL2", nullable = true)
     private String url2;
 
+    @Schema(description = "프로필 사진 URL", nullable = true)
     private String pictureUrl;
 
 }
