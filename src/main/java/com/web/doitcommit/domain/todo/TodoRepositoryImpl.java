1a71948bf87b29abb7be074f658e781c8fad6646
hyeongwoo-LEE <77663506+hyeongwoo-LEE@users.noreply.github.com>
2022-01-31 19:31:09 +0900

    todo - 투두리스트 사용자개수 정의 메소드 수정

--- src/main/java/com/web/doitcommit/domain/todo/TodoRepositoryImpl.java
+++ src/main/java/com/web/doitcommit/domain/todo/TodoRepositoryImpl.java
@@ -1,8 +1,16 @@
 package com.web.doitcommit.domain.todo;
 
+import com.querydsl.core.types.dsl.DatePath;
+import com.querydsl.core.types.dsl.DateTimePath;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.apache.el.lang.ExpressionBuilder;
 
 import javax.persistence.EntityManager;
+import java.text.DateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 
 public class TodoRepositoryImpl implements TodoRepositoryQuerydsl {
@@ -15,14 +23,20 @@ public class TodoRepositoryImpl implements TodoRepositoryQuerydsl {
 
     /**
      * 사용자 개수 지정 투두 리스트 조회
+     *
+     * 1. 당일날짜 투두, 회원별
+     * 2. 1) 상단고정순 2) 완료상태순 3) 작성순
      */
     @Override
     public List<Todo> getCustomLimitTodoList(int limit, Long memberId) {
         QTodo todo = QTodo.todo;
 
         return queryFactory.selectFrom(todo)
-                .where(todo.member.memberId.eq(memberId))
-                .orderBy(todo.isFixed.desc())
+                .where(todo.member.memberId.eq(memberId),
+                        todo.todoDateTime.year().eq(LocalDate.now().getYear()),
+                        todo.todoDateTime.month().eq(LocalDate.now().getMonthValue()),
+                        todo.todoDateTime.dayOfMonth().eq(LocalDate.now().getDayOfMonth()))
+                .orderBy(todo.isFixed.desc(), todo.isFinished.asc(), todo.todoId.asc())
                 .limit(limit)
                 .fetch();
     }
