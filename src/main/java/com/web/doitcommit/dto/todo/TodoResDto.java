4f66e02b2182bcadb551516c2f7f6152343b48d8
hyeongwoo-LEE <77663506+hyeongwoo-LEE@users.noreply.github.com>
2022-02-04 02:51:54 +0900

    swagger - todo 관련 dto 설명 추가

--- src/main/java/com/web/doitcommit/dto/todo/TodoResDto.java
+++ src/main/java/com/web/doitcommit/dto/todo/TodoResDto.java
@@ -2,6 +2,7 @@ package com.web.doitcommit.dto.todo;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.web.doitcommit.domain.todo.Todo;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -9,20 +10,28 @@ import java.time.LocalDateTime;
 @Data
 public class TodoResDto {
 
+    @Schema(description = "todo 고유값")
     private Long todoId;
 
+    @Schema(description = "제목")
     private String title;
 
+    @Schema(description = "본문")
     private String content;
 
+    @Schema(description = "투두타입")
     private String type;
 
+    @Schema(description = "중요도")
     private String importance;
 
+    @Schema(description = "상단고정 여부")
     private Boolean isFixed;
 
+    @Schema(description = "완료상태")
     private Boolean isFinished;
 
+    @Schema(description = "투두 날짜('yyyy/MM/ddTHH:mm')")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy/MM/dd'T'HH:mm", timezone = "Asia/Seoul")
     private LocalDateTime todoDateTime;
 
