c9ef87f25bd69a60f6baec7ec78595d3c04465c1
hyeongwoo-LEE <77663506+hyeongwoo-LEE@users.noreply.github.com>
2022-02-08 02:54:08 +0900

    feature: CustomException 클래스 생성

--- src/main/java/com/web/doitcommit/handler/ControllerExceptionHandler.java
+++ src/main/java/com/web/doitcommit/handler/ControllerExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.web.doitcommit.handler;
 
 import com.web.doitcommit.dto.CMRespDto;
+import com.web.doitcommit.handler.exception.CustomException;
 import com.web.doitcommit.handler.exception.CustomValidationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,8 +15,15 @@ public class ControllerExceptionHandler {
 
     //유효성 검사
     @ExceptionHandler(CustomValidationException.class)
-    public ResponseEntity<CMRespDto<?>> validationApiException(CustomValidationException e){
+    public ResponseEntity<CMRespDto<?>> validationException(CustomValidationException e){
 
         return new ResponseEntity<>(new CMRespDto<>(-1,e.getMessage(),e.errorMap), HttpStatus.BAD_REQUEST);
+    }
+
+    //에러 처리
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<CMRespDto<?>> apiException(CustomException e){
+
+        return new ResponseEntity<>(new CMRespDto<>(-1,e.getMessage(),null), HttpStatus.BAD_REQUEST);
     }
 }
