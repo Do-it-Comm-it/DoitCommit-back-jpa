69aeffa1ba8af99e01088bf6fd68619399fec479
hyeongwoo-LEE <77663506+hyeongwoo-LEE@users.noreply.github.com>
2022-02-08 03:05:35 +0900

    feature: customException 클래스 생성

---
+++ src/main/java/com/web/doitcommit/handler/exception/CustomException.java
@@ -0,0 +1,12 @@
+package com.web.doitcommit.handler.exception;
+
+
+public class CustomException extends RuntimeException{
+
+    private static final long serialVersionUID=1L;
+
+    public CustomException(String message) {
+        super(message);
+    }
+
+}
