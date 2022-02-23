60f5d27e54c79e0336a40fc29157a929a3450d4e
corazon4815 <69441691+corazon4815@users.noreply.github.com>
2022-02-23 22:06:33 +0900

    feat : board entity 생성

--- src/main/java/com/web/doitcommit/domain/board/Board.java
+++ src/main/java/com/web/doitcommit/domain/board/Board.java
@@ -1,12 +1,10 @@
 package com.web.doitcommit.domain.board;
 
 import com.web.doitcommit.domain.BaseEntity;
+import com.web.doitcommit.domain.member.Member;
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Builder
 @AllArgsConstructor
@@ -18,4 +16,21 @@ public class Board extends BaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long boardId;
+
+    @ManyToOne(cascade = CascadeType.MERGE, targetEntity = Member.class)
+    @JoinColumn(name = "member_id", updatable = false)
+    private Member member;
+
+    @Column(nullable = false)
+    private String categoryId;
+
+    @Column(nullable = false)
+    private String boardTitle;
+
+    @Column(columnDefinition = "TEXT")
+    private String boardContent;
+
+    @Builder.Default
+    @Column(nullable = false)
+    private int boardCnt = 0;
 }
---
+++ src/main/java/com/web/doitcommit/service/board/boardService.java
@@ -0,0 +1,4 @@
+package com.web.doitcommit.service.board;
+
+public class boardService {
+}
