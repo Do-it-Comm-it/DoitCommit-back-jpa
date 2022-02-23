09431e1beb105cf26dc93e4cac548d0c0cebb9af
hyeongwoo-LEE <77663506+hyeongwoo-LEE@users.noreply.github.com>
2022-02-23 18:04:40 +0900

    feature : Board 클래스 생성

---
+++ src/main/java/com/web/doitcommit/domain/board/Board.java
@@ -0,0 +1,21 @@
+package com.web.doitcommit.domain.board;
+
+import com.web.doitcommit.domain.BaseEntity;
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+@Entity
+public class Board extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long boardId;
+}
