68364fc6ac641d5ed665ba50b26ed1abacbc426d
hyeongwoo-LEE <77663506+hyeongwoo-LEE@users.noreply.github.com>
2022-02-23 19:29:28 +0900

    feature: Bookmark 도메인 설계

--- src/main/java/com/web/doitcommit/domain/board/Board.java
+++ src/main/java/com/web/doitcommit/domain/board/Board.java
@@ -1,12 +1,13 @@
 package com.web.doitcommit.domain.board;
 
 import com.web.doitcommit.domain.BaseEntity;
+import com.web.doitcommit.domain.bookmark.Bookmark;
 import lombok.*;
+import org.hibernate.annotations.BatchSize;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Builder
 @AllArgsConstructor
@@ -18,4 +19,8 @@ public class Board extends BaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long boardId;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "board", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+    private List<Bookmark> bookmarkList = new ArrayList<>();
 }
