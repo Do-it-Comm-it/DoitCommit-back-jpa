fb6e678f5f5faf13bf19e0e8493975e37fac7acf
hyeongwoo-LEE <77663506+hyeongwoo-LEE@users.noreply.github.com>
2022-02-23 18:26:24 +0900

    feature: Heart 생성

--- src/main/java/com/web/doitcommit/domain/heart/Heart.java
+++ src/main/java/com/web/doitcommit/domain/heart/Heart.java
@@ -1,11 +1,10 @@
 package com.web.doitcommit.domain.heart;
 
+import com.web.doitcommit.domain.board.Board;
+import com.web.doitcommit.domain.member.Member;
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Builder
 @AllArgsConstructor
@@ -13,8 +12,30 @@ import javax.persistence.Id;
 @Getter
 @ToString(exclude = {"member","board"})
 @Entity
+@Table(
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name="heart_uk",
+                        columnNames={"member_id", "board_id"}
+                )
+        }
+)
 public class Heart {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long heartId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id", nullable = false)
+    private Board board;
+
+    //연관관계 메서드
+    public void setBoard(Board board){
+        this.board = board;
+        board.getHeartList().add(this);
+    }
 }
