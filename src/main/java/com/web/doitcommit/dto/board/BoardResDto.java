4742e3e495fa711d71a6ffb118d2b6e6afdeefe3
corazon4815 <69441691+corazon4815@users.noreply.github.com>
2022-02-24 22:31:39 +0900

    feat : board dto 생성

---
+++ src/main/java/com/web/doitcommit/dto/board/BoardResDto.java
@@ -0,0 +1,59 @@
+package com.web.doitcommit.dto.board;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.web.doitcommit.domain.board.Board;
+import com.web.doitcommit.domain.todo.Todo;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Schema(description = "게시글 조회 dto")
+@Data
+public class BoardResDto {
+
+    @Schema(description = "게시글 번호")
+    private Long boardId;
+
+    @Schema(description = "닉네임")
+    private String nickname;
+
+    @Schema(description = "카테고리 아이디")
+    private String categoryId;
+
+    @Schema(description = "글제목")
+    private String boardTitle;
+
+    @Schema(description = "글내용")
+    private String boardContent;
+
+    @Schema(description = "조회수")
+    private int boardCnt;
+
+    @Schema(description = "태그", nullable = true)
+    private Set<String> tag = new HashSet<>();
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm", timezone = "Asia/Seoul")
+    @Schema(description = "등록 날짜", nullable = true)
+    private LocalDateTime regDate;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm", timezone = "Asia/Seoul")
+    @Schema(description = "수정 날짜", nullable = true)
+    private LocalDateTime modDate;
+
+    public BoardResDto(Board board){
+        boardId = board.getBoardId();
+        nickname = board.getMember().nickname;
+        tag = board.getTag();
+        categoryId = board.getCategoryId();
+        boardTitle = board.getBoardTitle();
+        boardContent = board.getBoardContent();
+        boardCnt = board.getBoardCnt();
+        regDate = board.getRegDate();
+        modDate = board.getModDate();
+    }
+
+}
