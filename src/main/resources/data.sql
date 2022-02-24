d25d380e9186d4252bc123fbc0ed7d6283a64825
corazon4815 <69441691+corazon4815@users.noreply.github.com>
2022-02-25 00:07:38 +0900

    feat : data.sql 파일 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,50 @@
+insert into member (nickname,oauth_id,password,provider,role) values ('test2','1111111','1111111','GOOGLE','USER');
+
+insert into board_category (category_name) values ('공지사항');
+insert into board_category (category_name) values ('커뮤니티');
+insert into board (board_cnt, board_title, board_content, category_id, member_id) values (0, '공지사항첫번째글제목', '첫번째글내용', 1, 1);
+insert into board (board_cnt, board_title, board_content, category_id, member_id) values (0, '공지사항두번째글제목', '두번째글제목', 1, 1);
+insert into board (board_cnt, board_title, board_content, category_id, member_id) values (0, '공지사항두번째글제목', '세번째글제목', 1, 1);
+insert into board (board_cnt, board_title, board_content, category_id, member_id) values (0, '글제목입니다', '글내용', 1, 1);
+insert into board (board_cnt, board_title, board_content, category_id, member_id) values (0, '글제목입니당', '글내용', 1, 1);
+insert into board (board_cnt, board_title, board_content, category_id, member_id) values (0, '제목', '글내용', 1, 1);
+insert into board (board_cnt, board_title, board_content, category_id, member_id) values (0, '공지사항글', '글내용', 1, 1);
+insert into board (board_cnt, board_title, board_content, category_id, member_id) values (0, '글제목', '글내용', 1, 1);
+insert into board (board_cnt, board_title, board_content, category_id, member_id) values (0, '타이틀', '글내용', 1, 1);
+insert into board (board_cnt, board_title, board_content, category_id, member_id) values (0, 'ㅇㅇㅇㅇㅇ', '글내용', 1, 1);
+insert into board (board_cnt, board_title, board_content, category_id, member_id) values (0, '제목', '글내용', 1, 1);
+insert into board (board_cnt, board_title, board_content, category_id, member_id) values (0, '공지사항글', '글내용', 1, 1);
+insert into board (board_cnt, board_title, board_content, category_id, member_id) values (0, '글제목', '글내용', 1, 1);
+insert into board (board_cnt, board_title, board_content, category_id, member_id) values (0, '타이틀', '글내용', 1, 1);
+insert into board (board_cnt, board_title, board_content, category_id, member_id) values (0, 'ㅇㅇㅇㅇㅇ', '글내용', 1, 1);
+
+insert into board (board_cnt, board_title, board_content, category_id, member_id) values (0, '커뮤니티첫번째글제목', '첫번째글내용', 2, 1);
+insert into board (board_cnt, board_title, board_content, category_id, member_id) values (0, '커뮤니티두번째글제목', '두번째글제목', 2, 1);
+insert into board (board_cnt, board_title, board_content, category_id, member_id) values (0, '공지사항세번째글제목', '세번째글제목', 2, 1);
+insert into board (board_cnt, board_title, board_content, category_id, member_id) values (0, '글제목입니다', '글내용', 2, 1);
+insert into board (board_cnt, board_title, board_content, category_id, member_id) values (0, '글제목입니당', '글내용', 2, 1);
+insert into board (board_cnt, board_title, board_content, category_id, member_id) values (0, '제목', '글내용', 2, 1);
+insert into board (board_cnt, board_title, board_content, category_id, member_id) values (0, '글', '글내용', 2, 1);
+insert into board (board_cnt, board_title, board_content, category_id, member_id) values (0, '글제목', '글내용', 2, 1);
+insert into board (board_cnt, board_title, board_content, category_id, member_id) values (0, '타이틀', '글내용', 2, 1);
+insert into board (board_cnt, board_title, board_content, category_id, member_id) values (0, 'ㅇㅇㅇㅇ', '글내용', 2, 1);
+insert into board (board_cnt, board_title, board_content, category_id, member_id) values (0, '글', '글내용', 2, 1);
+insert into board (board_cnt, board_title, board_content, category_id, member_id) values (0, '글제목', '글내용', 2, 1);
+insert into board (board_cnt, board_title, board_content, category_id, member_id) values (0, '타이틀', '글내용', 2, 1);
+insert into board (board_cnt, board_title, board_content, category_id, member_id) values (0, 'ㅇㅇㅇㅇ', '글내용', 2, 1);
+insert into board (board_cnt, board_title, board_content, category_id, member_id) values (0, '글제목', '글내용', 2, 1);
+insert into board (board_cnt, board_title, board_content, category_id, member_id) values (0, '타이틀', '글내용', 2, 1);
+insert into board (board_cnt, board_title, board_content, category_id, member_id) values (0, 'ㅇㅇㅇㅇ', '글내용', 2, 1);
+
+insert into board_tag (board_id, tag) values (176, '직장인');
+insert into board_tag (board_id, tag) values (176, '공대생');
+insert into board_tag (board_id, tag) values (176, '취준생');
+insert into board_tag (board_id, tag) values (177, '직장인');
+insert into board_tag (board_id, tag) values (177, '공대생');
+insert into board_tag (board_id, tag) values (177, '취준생');
+insert into board_tag (board_id, tag) values (178, '직장인');
+insert into board_tag (board_id, tag) values (178, '공대생');
+insert into board_tag (board_id, tag) values (178, '취준생');
+
+
+
