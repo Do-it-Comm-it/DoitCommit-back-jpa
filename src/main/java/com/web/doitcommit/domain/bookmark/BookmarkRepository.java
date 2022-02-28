7f41b5e93014ac89fa21340061f7b851fb6ca3e5
hyeongwoo-LEE <77663506+hyeongwoo-LEE@users.noreply.github.com>
2022-02-28 17:02:02 +0900

    feature : 북마크 생성, 취소 기능

--- src/main/java/com/web/doitcommit/domain/bookmark/BookmarkRepository.java
+++ src/main/java/com/web/doitcommit/domain/bookmark/BookmarkRepository.java
@@ -1,6 +1,14 @@
 package com.web.doitcommit.domain.bookmark;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface BookmarkRepository extends JpaRepository<Bookmark,Long> {
+
+    @Modifying(clearAutomatically = true)
+    @Query("delete from Bookmark b where b.member.memberId = :memberId and b.board.boardId = :boardId")
+    void deleteBookmark(@Param("memberId") Long memberId, @Param("boardId") Long boardId);
+
 }
