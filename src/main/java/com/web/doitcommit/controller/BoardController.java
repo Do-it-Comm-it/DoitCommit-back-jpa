71dc48481db7d04fb4195bb6cd3c75812b01c969
corazon4815 <69441691+corazon4815@users.noreply.github.com>
2022-03-02 23:31:49 +0900

    feat : BoardController 게시물 등록 api (진행중)

--- src/main/java/com/web/doitcommit/controller/BoardController.java
+++ src/main/java/com/web/doitcommit/controller/BoardController.java
@@ -1,11 +1,12 @@
 package com.web.doitcommit.controller;
 
+import com.web.doitcommit.config.auth.PrincipalDetails;
 import com.web.doitcommit.dto.CMRespDto;
+import com.web.doitcommit.dto.board.BoardRegDto;
 import com.web.doitcommit.dto.board.BoardResDto;
-import com.web.doitcommit.dto.member.MemberInfoDto;
-import com.web.doitcommit.dto.todo.TodoResDto;
 import com.web.doitcommit.service.board.BoardService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -14,7 +15,10 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import javax.validation.Valid;
 import java.util.List;
 
 @RequestMapping("/board")
@@ -24,6 +28,9 @@ public class BoardController {
 
     private final BoardService boardService;
 
+    /**
+     * 게시판 목록 조회
+     */
     @Operation(summary = "게시판 목록 조회 API", description = "게시판 목록 정보를 조회한다.")
     @ApiResponses({
             @ApiResponse(responseCode = "200", content = @Content(array = @ArraySchema(schema = @Schema(implementation = BoardResDto.class)))),
@@ -38,6 +45,25 @@ public class BoardController {
             @RequestParam(value = "pageSize") int pageSize) {
         List<BoardResDto> boardResDtoList = boardService.getBoardList(pageNo, pageSize);
         return new ResponseEntity<>(new CMRespDto<>(1, "게시판 리스트 조회 성공", boardResDtoList),HttpStatus.OK);
+    }
+
+    @Operation(summary = "게시글 등록 API", description = "게시글을 등록한다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(example = "{\n" +
+                    "  \"message\": \"게시글 등록 성공\",\n" +
+                    "  \"data\": null,\n" +
+                    "  \"code\": 1\n" +
+                    "}"))),
+            @ApiResponse(responseCode = "400", content = @Content(schema = @Schema(example = "{\"error\": \"Bad Request\"}"))),
+            @ApiResponse(responseCode = "401", content = @Content(schema = @Schema(example = "{\"error\": \"Unauthorized\"}"))),
+            @ApiResponse(responseCode = "404", content = @Content(schema = @Schema(example = "{\"error\": \"Not Found\"}"))),
+            @ApiResponse(responseCode = "500",  content = @Content(schema = @Schema(example = "{\"error\": \"Internal Server Error\"}")))
+    })
+    @PostMapping("")
+    public ResponseEntity<?> createBoard(@Valid @RequestBody BoardRegDto boardRegDto, BindingResult bindingResult,
+                                         @Parameter(hidden = true) @AuthenticationPrincipal PrincipalDetails principalDetails){
+        BoardResDto boardResDto = boardService.createBoard(boardRegDto, principalDetails.getMember().getMemberId());
+        return new ResponseEntity<>(new CMRespDto<>(1, "게시글 등록 성공", boardResDto),HttpStatus.OK);
     }
 
 
