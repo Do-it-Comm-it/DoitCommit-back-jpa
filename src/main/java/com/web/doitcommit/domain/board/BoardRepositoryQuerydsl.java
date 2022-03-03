4173a66496d430991475101478621d5928ba3190
corazon4815 <69441691+corazon4815@users.noreply.github.com>
2022-03-03 22:39:45 +0900

    feat : tag 목록 조회 repo 생성

--- src/main/java/com/web/doitcommit/domain/board/BoardRepositoryQuerydsl.java
+++ src/main/java/com/web/doitcommit/domain/board/BoardRepositoryQuerydsl.java
@@ -1,7 +1,18 @@
 package com.web.doitcommit.domain.board;
 
+import com.web.doitcommit.domain.TagCategory;
+
 import java.util.List;
 
 public interface BoardRepositoryQuerydsl {
+
+    /**
+     * 게시판 목록 조회
+     */
     List<Board> getCustomBoardList(int pageNo, int pageSize);
+
+    /**
+     * 태그 목록 조회
+     */
+    List<String> getCustomTagList();
 }
