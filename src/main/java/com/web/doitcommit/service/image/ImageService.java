99bb6335b302440aadb07e5fce7ea00d65aeeb08
corazon4815 <69441691+corazon4815@users.noreply.github.com>
2022-03-06 15:49:22 +0900

    feat : 이미지 S3 url 변환 api service 개발

--- src/main/java/com/web/doitcommit/service/image/ImageService.java
+++ src/main/java/com/web/doitcommit/service/image/ImageService.java
@@ -1,16 +1,15 @@
 package com.web.doitcommit.service.image;
 
-import com.web.doitcommit.domain.files.ImageRepository;
-import com.web.doitcommit.domain.files.MemberImage;
-import com.web.doitcommit.domain.files.MemberImageRepository;
+import com.web.doitcommit.domain.board.Board;
+import com.web.doitcommit.domain.files.*;
 import com.web.doitcommit.domain.member.Member;
 import com.web.doitcommit.s3.S3Uploader;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 @Transactional
@@ -19,6 +18,7 @@ import java.util.Map;
 public class ImageService {
 
     private final MemberImageRepository memberImageRepository;
+    private final BoardImageRepository boardImageRepository;
     private final S3Uploader s3Uploader;
     private final ImageRepository imageRepository;
 
@@ -29,9 +29,6 @@ public class ImageService {
     public Long imageMemberRegister(Member member, MultipartFile imageFile) throws IOException {
 
         Map<String, String> fileMap = s3Uploader.S3Upload(imageFile);
-        System.out.println("-----------------");
-        System.out.println(fileMap.get("filePath"));
-        System.out.println(fileMap.get("fileNm"));
 
         MemberImage memberImage = new MemberImage(member, fileMap.get("filePath"), fileMap.get("fileNm"));
 
@@ -39,6 +36,28 @@ public class ImageService {
 
         return memberImage.getImageId();
     }
+
+    /**
+     * 게시글 이미지 저장 및 s3 url 반환
+     */
+    @Transactional
+    public Map<String, Object> imageBoardRegister(MultipartFile imageFile) throws IOException {
+        Map<String, String> fileMap = s3Uploader.S3Upload(imageFile);
+        String filePath = fileMap.get("filePath");
+        String fileNm = fileMap.get("fileNm");
+
+        Board board = new Board();
+        board.setBoardId(1L);
+
+        BoardImage boardImage = new BoardImage(board, filePath, fileNm);
+        boardImageRepository.save(boardImage);
+
+        Map<String, Object> resultMap = new HashMap<>();
+        resultMap.put("url", getImage(filePath, fileNm));
+        resultMap.put("imageId", boardImage.getImageId());
+        return resultMap;
+    }
+
 
 
     /**
