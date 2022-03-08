507b584bd52f5b2d677b01b4331475de75677e5f
hyeongwoo-LEE <77663506+hyeongwoo-LEE@users.noreply.github.com>
2022-03-09 03:25:50 +0900

    feature : 최근 7일간의 인기태그 리스트 조회

--- src/main/java/com/web/doitcommit/domain/board/BoardRepository.java
+++ src/main/java/com/web/doitcommit/domain/board/BoardRepository.java
@@ -1,6 +1,27 @@
 package com.web.doitcommit.domain.board;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface BoardRepository extends JpaRepository<Board,Long>, BoardRepositoryQuerydsl {
+
+    /**
+     * 7일간의 인기태그 상위 8개 리스트
+     */
+    @Query(value = "select bt.tag, count(bt.tag) as cnt from board b join board_tag bt on bt.board_id = b.board_id " +
+            "where b.mod_date BETWEEN DATE_ADD(NOW(),INTERVAL -7 DAY ) AND NOW()" +
+            "group by bt.tag " +
+            "order by cnt desc " +
+            "limit 8", nativeQuery = true)
+    List<Object[]> getLimitPopularTag();
+
+    /**
+     * 7일간의 인기태그 리스트
+     */
+    @Query(value = "select bt.tag, count(bt.tag) as cnt from board b join board_tag bt on bt.board_id = b.board_id " +
+            "where b.mod_date BETWEEN DATE_ADD(NOW(),INTERVAL -7 DAY ) AND NOW()" +
+            "group by bt.tag ", nativeQuery = true)
+    List<Object[]> getAllPopularTag();
 }
