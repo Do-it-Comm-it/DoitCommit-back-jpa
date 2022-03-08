b19400a7559d4f2d492d473f874fbbbe84e0b32a
corazon4815 <69441691+corazon4815@users.noreply.github.com>
2022-03-09 02:50:24 +0900

    feat : ImageRegDto 생성

---
+++ src/main/java/com/web/doitcommit/dto/board/ImageRegDto.java
@@ -0,0 +1,18 @@
+package com.web.doitcommit.dto.board;
+
+import lombok.Getter;
+
+@Getter
+public class ImageRegDto {
+
+    private String fileNm;
+    private String filePath;
+
+    public ImageRegDto(){}
+
+    public ImageRegDto(String fileNm, String filePath){
+        this.fileNm = fileNm;
+        this.filePath = filePath;
+
+    }
+}
