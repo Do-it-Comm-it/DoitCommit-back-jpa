7e3b73259b3232540a6581ca23d4da3cea5a3906
corazon4815 <69441691+corazon4815@users.noreply.github.com>
2022-03-10 22:10:38 +0900

    refactor : 게시글등록, s3이미지 삭제, db이미지 등록으로 분리

--- src/main/java/com/web/doitcommit/service/board/BoardService.java
+++ src/main/java/com/web/doitcommit/service/board/BoardService.java
@@ -2,16 +2,14 @@ package com.web.doitcommit.service.board;
 
 import com.web.doitcommit.domain.board.Board;
 import com.web.doitcommit.domain.board.BoardRepository;
-import com.web.doitcommit.domain.files.BoardImageRepository;
 import com.web.doitcommit.dto.board.BoardRegDto;
+import com.web.doitcommit.dto.board.BoardListResDto;
 import com.web.doitcommit.dto.board.BoardResDto;
 import com.web.doitcommit.dto.board.ImageRegDto;
 import com.web.doitcommit.service.image.ImageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.io.IOException;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -28,65 +26,96 @@ public class BoardService {
      * 게시판 목록 조회
      */
     @Transactional(readOnly = true)
-    public List<BoardResDto> getBoardList(int pageNo, int pageSize) {
+    public List<BoardListResDto> getBoardList(int pageNo, int pageSize) {
         List<Board> result = boardRepository.getCustomBoardList(pageNo, pageSize);
-        return result.stream().map(board -> new BoardResDto(board)).collect(Collectors.toList());
+        return result.stream().map(board -> new BoardListResDto(board)).collect(Collectors.toList());
     }
 
     /**
      * 게시글 등록
      */
-    public void createBoard(BoardRegDto boardRegDto, Long principalId) throws IOException {
+    @Transactional
+    public void createBoard(BoardRegDto boardRegDto, Long principalId) {
         Board boardEntity = boardRegDto.toEntity(principalId);
-        boardRepository.save(boardEntity);
+        Board board = boardRepository.save(boardEntity);
 
-        //등록안한 이미지 삭제 시작
-        ImageRegDto[] allImageArr = boardRegDto.getAllImageArr();
-        ImageRegDto[] imageArr = boardRegDto.getImageArr();
+        ImageRegDto[] allImageArr = new ImageRegDto[0];
+        if(boardRegDto.getAllImageArr() !=null){
+            allImageArr = boardRegDto.getAllImageArr();
+        }
 
-        List<String> imageList = new ArrayList<String>();
+        ImageRegDto[] imageArr = new ImageRegDto[0];
+        if(boardRegDto.getImageArr() !=null){
+            imageArr = boardRegDto.getImageArr();
+        }
+
+        //S3에서 이미지 삭제
+        if(allImageArr.length != 0) {
+            removeImage(allImageArr, imageArr);
+        }
+        //DB에 이미지 등록
+        if(imageArr.length != 0) {
+            registerImage(board, imageArr);
+        }
+    }
+
+    /**
+     * S3에서 이미지 삭제
+     */
+    @Transactional
+    public void removeImage(ImageRegDto[] allImageArr,
+                            ImageRegDto[] imageArr) {
         List<String> allImageList = new ArrayList<String>();
-
-        for(int i = 0; i< allImageArr.length; i++) {
-            allImageList.add(allImageArr[i].getFilePath()+"/"+allImageArr[i].getFileNm());
+        List<String> imageList = new ArrayList<String>();
+        //반환한 전체 url list
+        for (int i = 0; i < allImageArr.length; i++) {
+            allImageList.add(allImageArr[i].getFilePath() + "/" + allImageArr[i].getFileNm());
         }
-        for(int i = 0; i< imageArr.length; i++) {
-            imageList.add(imageArr[i].getFilePath()+"/"+imageArr[i].getFileNm());
+        //등록한 url list
+        for (int i = 0; i < imageArr.length; i++) {
+            imageList.add(imageArr[i].getFilePath() + "/" + imageArr[i].getFileNm());
         }
-        System.out.println(allImageList);
-        System.out.println(imageList);
+        //S3에서 삭제해야할 url
         allImageList.removeAll(imageList);
-        if(allImageList.size() !=0){
-            for(int i = 0; i< allImageList.size(); i++) {
+        if (allImageList.size() != 0) {
+            for (int i = 0; i < allImageList.size(); i++) {
                 imageService.imageRemove(allImageList.get(i));
             }
         }
+    }
 
-        //등록안한 이미지 삭제 끝
-
-        //db에 이미지 등록
-        if(imageArr.length != 0){
-            imageService.imageBoardDbRegister(boardEntity, imageArr);
+    /**
+     * DB에 이미지 등록
+     */
+    @Transactional
+    public void registerImage(Board board, ImageRegDto[] imageArr) {
+        if (imageArr.length != 0) {
+            imageService.imageBoardDbRegister(board, imageArr);
         }
+    }
 
-
+    /**
+     * 게시글 조회
+     */
+    @Transactional
+    public BoardResDto GetBoard(Long boardId) {
+        Board boardEntity = boardRepository.findById(boardId).orElseThrow(() ->
+                new IllegalArgumentException("존재하지않는 게시글입니다."));
+        boardEntity.changeBoardCnt();
+        return new BoardResDto(boardEntity);
     }
 
     /**
      * 태그 목록 조회
      */
     @Transactional(readOnly = true)
-    public String[] getBoardList() {
+    public String[] getBoardTagList() {
         List<String> result = boardRepository.getCustomTagList();
         String[] tagArr = new String[result.size()];
         int size = 0;
-        for(String temp : result){
+        for (String temp : result) {
             tagArr[size++] = temp;
         }
         return tagArr;
     }
-
-
-
-
 }
