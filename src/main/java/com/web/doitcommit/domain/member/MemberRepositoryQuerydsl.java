ba3e04c2d5ba09a4e6f7dab4518646dd5537dc3e
corazon4815 <69441691+corazon4815@users.noreply.github.com>
2022-08-30 22:35:39 +0900

    feat : 관심기술 목록 조회 쿼리 추가

--- src/main/java/com/web/doitcommit/domain/member/MemberRepositoryQuerydsl.java
+++ src/main/java/com/web/doitcommit/domain/member/MemberRepositoryQuerydsl.java
@@ -1,10 +1,17 @@
 package com.web.doitcommit.domain.member;
 
+import java.util.List;
+
 public interface MemberRepositoryQuerydsl {
 
     /**
      * 멤버 이미지 url 조회
      */
     String getMemberImage(Long memberId);
+
+    /**
+     * 관심기술 목록 조회
+     */
+    List getCustomInterestTechList(Long boardId);
 
 }
