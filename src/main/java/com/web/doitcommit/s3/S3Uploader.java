28601ca97f64bcb7463b109ce01d2d9ee719c9dc
hyeongwoo-LEE <77663506+hyeongwoo-LEE@users.noreply.github.com>
2022-11-20 20:38:44 +0900

    eb

--- src/main/java/com/web/doitcommit/s3/S3Uploader.java
+++ src/main/java/com/web/doitcommit/s3/S3Uploader.java
@@ -25,8 +25,8 @@ import java.util.*;
 @Component
 public class S3Uploader {
 
-    //@Value("${cloud.aws.s3.bucket}")
-    private final String bucket = "doitcommit20221120";
+    @Value("${cloud.aws.s3.bucket}")
+    private final String bucket;
 
     private final AmazonS3 amazonS3;
     private final ImageRepository imageRepository;
